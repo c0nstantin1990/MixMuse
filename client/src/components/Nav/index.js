9b7ce6320fd7785e19b6aae6d64deda9506e59ea
c0nstantin1990 <118696575+c0nstantin1990@users.noreply.github.com>
2023-09-20 21:33:46 -0400

    added server file

---
+++ client/src/components/Nav/index.js
@@ -0,0 +1,50 @@
+import React from "react";
+import Auth from "../../utils/auth";
+import { Link } from "react-router-dom";
+
+function Nav() {
+  function showNavigation() {
+    if (Auth.loggedIn()) {
+      return (
+        <ul className="flex-row">
+          <li className="mx-1">
+            <Link to="/orderHistory">Order History</Link>
+          </li>
+          <li className="mx-1">
+            <a href="/" onClick={() => Auth.logout()}>
+              Logout
+            </a>
+          </li>
+        </ul>
+      );
+    } else {
+      return (
+        <ul className="flex-row">
+          <li className="mx-1">
+            <Link to="/signup">Signup</Link>
+          </li>
+          <li className="mx-1">
+            <Link to="/login">Login</Link>
+          </li>
+        </ul>
+      );
+    }
+  }
+
+  return (
+    <header style={{ background: "var(--primary)" }} className="flex-row px-1">
+      <h1>
+        <Link to="/" style={{ color: "var(--light)" }}>
+          <span role="img" aria-label="shopping bag">
+            🍹
+          </span>
+          Drink Me
+        </Link>
+      </h1>
+
+      <nav>{showNavigation()}</nav>
+    </header>
+  );
+}
+
+export default Nav;
