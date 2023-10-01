0f648a6ddef3f930970af5125ff1ce42408cbacc
DonnaThompson7 <130340823+DonnaThompson7@users.noreply.github.com>
2023-10-01 13:44:34 -0400

    enhanced ratings front-end functionality, fixed images

--- client/src/pages/Features.js
+++ client/src/pages/Features.js
@@ -5,91 +5,91 @@ function FeaturedProducts() {
     <div style={{ height: "100px !important" }}>
       <div
         id="myCarousel"
-        class=" carousel slide carousel-fade"
+        className=" carousel slide carousel-fade"
         data-bs-ride="carousel"
       >
-        <div class="carousel-inner">
-          <div class="carousel-item main-carousel-item active">
+        <div className="carousel-inner">
+          <div className="carousel-item main-carousel-item active">
             <div
-              class="overlay-image"
+              className="overlay-image"
               style={{
                 backgroundImage:
                   "url('http://cdnimg.webstaurantstore.com/uploads/design/2014/1/bartender-school.jpg')",
               }}
             ></div>
             <div
-              class=" container main-page-container "
+              className=" container main-page-container "
               style={{ marginTop: "60px" }}
             >
-              <h1 class="fw-bold">Check Out Our Featured Liquor</h1>
-              <p class="fs-3">
+              <h1 className="fw-bold">Check Out Our Featured Liquor</h1>
+              <p className="fs-3">
                 Click below to search featured spirits for your next cocktail
                 creation
               </p>
-              <a href="/categoryMenu" class="btn btn-outline-light">
+              <a href="/categoryMenu" className="btn btn-outline-light">
                 Featured Liquor
               </a>
             </div>
           </div>
-          <div class="carousel-item main-carousel-item">
+          <div className="carousel-item main-carousel-item">
             <div
-              class="overlay-image"
+              className="overlay-image"
               style={{
                 backgroundImage: `url("https://cdn.webshopapp.com/shops/273775/files/332466292/cocktails.jpg")`,
               }}
             ></div>
             <div
-              class="container main-page-container "
+              className="container main-page-container "
               style={{ marginTop: "60px" }}
             >
-              <h1 class="fw-bold">Check Out Our Featured Equipment</h1>
-              <p class="fs-3">
+              <h1 className="fw-bold">Check Out Our Featured Equipment</h1>
+              <p className="fs-3">
                 Click below to search featured equipment and build your dream
                 bar today
               </p>
-              <a href="/categoryMenu" class="btn btn-outline-light">
+              <a href="/categoryMenu" className="btn btn-outline-light">
                 Featured Equipment
               </a>
             </div>
           </div>
-          <div class="carousel-item main-carousel-item">
+          <div className="carousel-item main-carousel-item">
             <div
-              class="overlay-image"
+              className="overlay-image"
               style={{
                 backgroundImage: `url("https://jimymacspub.com/wp-content/uploads/2019/11/screen-shot-2019-11-26-at-10.19.27-am.jpg")`,
               }}
             ></div>
             <div
-              class="container main-page-container"
+              className="container main-page-container"
               style={{ marginTop: "60px" }}
             >
-              <h1 class="fw-bold">Check Out Our Featured Mixers</h1>
-              <p class="fs-3">
+              <h1 className="fw-bold">Check Out Our Featured Mixers</h1>
+              <p className="fs-3">
                 Click below to search featured mixers for your next cocktail
                 creation
               </p>
-              <a href="/categoryMenu" class="btn btn-outline-light">
+              <a href="/categoryMenu" className="btn btn-outline-light">
                 Feataured Mixers
               </a>
             </div>
           </div>
-          <div class="carousel-item main-carousel-item">
+          <div className="carousel-item main-carousel-item">
             <div
-              class="overlay-image"
+              className="overlay-image"
               style={{
                 backgroundImage: `url("https://bartendersbusiness.com/en/articles/images/w/1200/BartendersBusiness-01222019060923000000-5c46b393188eb.jpg")`,
               }}
             ></div>
             <div
-              class="container main-page-container"
+              className="container main-page-container"
               style={{ marginTop: "60px" }}
             >
-              <h1 class="fw-bold">Rate and Review Products Here</h1>
-              <p class="fs-3">
+              <h1 className="fw-bold">Rate and Review Products Here</h1>
+              <p className="fs-3">
                 Leave your thoughts on products and share your insight with
                 other bartenders
               </p>
-              <a href="/categoryMenu" class="btn btn-outline-light">
+              <a href="/categoryMenu" className="btn btn-outline-light">
                 Leave your thoughts!
               </a>
             </div>
@@ -97,21 +97,21 @@ function FeaturedProducts() {
         </div>
         <a
           href="#myCarousel"
-          class="carousel-control-prev"
+          className="carousel-control-prev"
           role="button"
           data-bs-slide="prev"
         >
-          <span class="sr-only"></span>
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="sr-only"></span>
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
         </a>
         <a
           href="#myCarousel"
-          class="carousel-control-next"
+          className="carousel-control-next"
           role="button"
           data-bs-slide="next"
         >
-          <span class="sr-only"></span>
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="sr-only"></span>
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
         </a>
       </div>
     </div>
