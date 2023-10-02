42f2030d1d6a2dc8d769baa97bd482a9a977f3ed
c0nstantin1990 <118696575+c0nstantin1990@users.noreply.github.com>
2023-10-01 22:07:47 -0400

    changed src

--- client/src/pages/Features.js
+++ client/src/pages/Features.js
@@ -1,4 +1,8 @@
 import React from "react";
+import main1 from "../assets/main1.jpeg";
+import main2 from "../assets/main2.jpeg";
+import main3 from "../assets/main3.jpeg";
+import main4 from "../assets/main4.jpeg";
 
 function FeaturedProducts() {
   return (
@@ -13,29 +17,30 @@ function FeaturedProducts() {
             <div
               className="overlay-image"
               style={{
-                backgroundImage:
-                  "url('http://cdnimg.webstaurantstore.com/uploads/design/2014/1/bartender-school.jpg')",
+                backgroundImage: `url(${main1})`,
               }}
             ></div>
             <div
-              className=" container main-page-container "
+              className=" container main-page-container test1 "
               style={{ marginTop: "60px" }}
             >
-              <h1 className="fw-bold">Check Out Our Featured Liquor</h1>
+             <div className="test1">
+              <h1 className="fw-bold" >Check Out Our Featured Liquor</h1>
               <p className="fs-3">
                 Click below to search featured spirits for your next cocktail
                 creation
               </p>
-              <a href="/categoryMenu" className="btn btn-outline-light">
+              </div>
+              {/* <a href="/categoryMenu" className="btn btn-outline-light">
                 Featured Liquor
-              </a>
+              </a> */}
             </div>
           </div>
           <div className="carousel-item main-carousel-item">
             <div
               className="overlay-image"
               style={{
-                backgroundImage: `url("https://cdn.webshopapp.com/shops/273775/files/332466292/cocktails.jpg")`,
+                backgroundImage: `url(${main2})`,
               }}
             ></div>
             <div
@@ -47,51 +52,52 @@ function FeaturedProducts() {
                 Click below to search featured equipment and build your dream
                 bar today
               </p>
-              <a href="/categoryMenu" className="btn btn-outline-light">
+              {/* <a href="/categoryMenu" className="btn btn-outline-light">
                 Featured Equipment
-              </a>
+              </a> */}
             </div>
           </div>
           <div className="carousel-item main-carousel-item">
             <div
               className="overlay-image"
               style={{
-                backgroundImage: `url("https://jimymacspub.com/wp-content/uploads/2019/11/screen-shot-2019-11-26-at-10.19.27-am.jpg")`,
+                backgroundImage: `url(${main3})`,
               }}
             ></div>
             <div
               className="container main-page-container"
               style={{ marginTop: "60px" }}
             >
+              
               <h1 className="fw-bold">Check Out Our Featured Mixers</h1>
               <p className="fs-3">
                 Click below to search featured mixers for your next cocktail
                 creation
               </p>
-              <a href="/categoryMenu" className="btn btn-outline-light">
+              {/* <a href="/categoryMenu" className="btn btn-outline-light">
                 Featured Mixers
-              </a>
+              </a> */}
             </div>
           </div>
           <div className="carousel-item main-carousel-item">
             <div
               className="overlay-image"
               style={{
-                backgroundImage: `url("https://bartendersbusiness.com/en/articles/images/w/1200/BartendersBusiness-01222019060923000000-5c46b393188eb.jpg")`,
+                backgroundImage: `url(${main4})`,
               }}
             ></div>
             <div
               className="container main-page-container"
               style={{ marginTop: "60px" }}
             >
-              <h1 className="fw-bold">Rate and Review Products Here</h1>
-              <p className="fs-3">
+              <h1 className="fw-bold overlay">Rate and Review Products Here</h1>
+              <p className="fs-3 overlay">
                 Leave your thoughts on products and share your insight with
                 other bartenders
               </p>
-              <a href="/categoryMenu" className="btn btn-outline-light">
+              {/* <a href="/categoryMenu" className="btn btn-outline-light">
                 Leave your thoughts!
-              </a>
+              </a> */}
             </div>
           </div>
         </div>
@@ -102,7 +108,10 @@ function FeaturedProducts() {
           data-bs-slide="prev"
         >
           <span className="sr-only"></span>
-          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span
+            className="carousel-control-prev-icon"
+            aria-hidden="true"
+          ></span>
         </a>
         <a
           href="#myCarousel"
@@ -111,7 +120,10 @@ function FeaturedProducts() {
           data-bs-slide="next"
         >
           <span className="sr-only"></span>
-          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span
+            className="carousel-control-next-icon"
+            aria-hidden="true"
+          ></span>
         </a>
       </div>
     </div>
